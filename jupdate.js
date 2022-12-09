f4e1ec0c6b2a30fe63368ed8ff1400b8bea7c3aa
madeforlosers <87035377+madeforlosers@users.noreply.github.com>
2022-12-09 09:55:45 -0600

    Update jupdate.js

--- jupdate.js
+++ jupdate.js
@@ -1,13 +1,21 @@
 const request = require('request');
-const fs = require("fs")
-console.log("Checking for updates...")
-gg = parseInt(fs.readFileSync("jbasher.js", "utf-8").split("\n")[0].split("version ")[1])
-request.get('https://raw.githubusercontent.com/madeforlosers/James-Archives/main/jBashInterpreter/jbasher.js', function(err, res, body) {
-  if(body.split("\n")[0].split("version ")[1] != undefined && parseInt(body.split("\n")[0].split("version ")[1]) > gg ){
-  console.log("Version "+body.split("\n")[0].split("version ")[1]+" is avaiable. overwriting jbasher.js..")
+const prompt = require('prompt-sync')();
+const fs = require('fs');
+gg = parseInt(fs.readFileSync("jbasher.js", "utf-8").split("\n")[4].replace(/\D+/g, ""))
+request.get('https://raw.githubusercontent.com/madeforlosers/jBasher/main/jbasher.js', function(err, res, body) {
+  if (parseInt(body.split("\n")[4].replace(/\D+/g, "")) != "" && parseInt(body.split("\n")[4].replace(/\D+/g, "")) > gg) {
+   console.log("Version "+body.split("\n")[0].split("version ")[1]+" is avaiable. overwriting jbasher.js..")
     fs.writeFileSync("jbasher.js", body, {flag: "w+"})
-    console.log("updated.")
-  }else{
-    console.log("jBasher is up to date. ")
+    console.log("updated jbasher.js")
+    updateReadme()
+  } else{
+    console.log("seems to be up to date.")
   }
 })
+function updateReadme(){
+request.get('https://raw.githubusercontent.com/madeforlosers/jBasher/main/readme.md', function(err, res, body) {
+    console.log("updating readme.md")
+   fs.writeFileSync("readme.md", body, {flag: "w+"})
+    console.log("updated")
+})
+}
